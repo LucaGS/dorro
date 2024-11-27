8b82f0b060cc1e5844f148d87806856a85ad9045
Luca Stieme <123728529+LucaGS@users.noreply.github.com>
2024-11-27 21:48:40 +0100

    revert

--- index.php
+++ index.php
@@ -1,18 +1,9 @@
 <?php
-// Verbindungsdetails
-$servername = getenv('AZURE_MYSQL_HOST'); // Hostname von Azure DB
-$username = getenv('AZURE_MYSQL_USERNAME'); // Benutzername
-$password = getenv('AZURE_MYSQL_PASSWORD'); // Passwort
-$dbname = getenv('AZURE_MYSQL_DBNAME'); // Datenbankname
-$port = getenv('AZURE_MYSQL_PORT'); // Port (standardmäßig 3306)
-
-// Verbindung mit MySQL herstellen
-$conn = new mysqli($servername, $username, $password, $dbname, $port);
-
-// Überprüfen, ob die Verbindung erfolgreich war
-if ($conn->connect_error) {
-    die("Verbindung fehlgeschlagen: " . $conn->connect_error);
-} 
-
-echo "Erfolgreiche Verbindung zur MySQL-Datenbank!";
+// Überprüfen, ob der Parameter 'name' in der URL übergeben wurde
+if (isset($_GET['name'])) {
+    $name = $_GET['name'];  // Den Wert des Parameters 'name' auslesen
+    echo "Hallo, " . htmlspecialchars($name) . "!";
+} else {
+    echo "Bitte gib deinen Namen in der URL an, z. B. ?name=John";
+}
 ?>
