60080b7209f9254a6fa72c5a90ecf9bc8e2d6ec7
Luca Stieme <123728529+LucaGS@users.noreply.github.com>
2024-11-27 21:18:15 +0100

    Update index.php
    123

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
 <?php
-echo "PHP funktioniert!";
-
+// Überprüfen, ob der Parameter 'name' in der URL übergeben wurde
+if (isset($_GET['name'])) {
+    $name = $_GET['name'];  // Den Wert des Parameters 'name' auslesen
+    echo "Hallo, " . htmlspecialchars($name) . "!";
+} else {
+    echo "Bitte gib deinen Namen in der URL an, z. B. ?name=John";
+}
+?>
