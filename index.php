891531257952b33a49f65ec32626279f267b0d8b
Luca Stieme <123728529+LucaGS@users.noreply.github.com>
2024-11-27 22:17:10 +0100

    123

--- index.php
+++ index.php
@@ -6,4 +6,8 @@ if (isset($_GET['name'])) {
 } else {
     echo "Bitte gib deinen Namen in der URL an, z. B. ?name=John";
 }
+
+$con = mysqli_init();
+mysqli_ssl_set($con,NULL,NULL, "C:\dev\dorro\DigiCertGlobalRootCA.crt.pem", NULL, NULL);
+mysqli_real_connect($conn, "dorro-server.mysql.database.azure.com", "svvaolmvjl", "{V6G52dgRBBQd$64$}", "{d-database}", 3306, MYSQLI_CLIENT_SSL);
 ?>
