981785b021136c760a84aa135aad73585f6a1523
Luca Stieme <123728529+LucaGS@users.noreply.github.com>
2024-11-28 18:46:01 +0100

    123

--- index.php
+++ index.php
@@ -9,6 +9,8 @@ if (isset($_GET['name'])) {
 
 $con = mysqli_init();
 mysqli_ssl_set(mysql: $con,key: NULL,certificate: NULL, ca_certificate: "C:\dev\dorro\DigiCertGlobalRootCA.crt.pem", ca_path: NULL, cipher_algos: NULL);
-mysqli_real_connect(mysql: $conn, hostname: "dorro-server.mysql.database.azure.com", username: "svvaolmvjl", password: "{V6G52dgRBBQd$64$}", database: "{d-database}", port: 3306, socket: MYSQLI_CLIENT_SSL);
+if(mysqli_real_connect(mysql: $conn, hostname: "dorro-server.mysql.database.azure.com", username: "svvaolmvjl", password: "{V6G52dgRBBQd$64$}", database: "{d-database}", port: 3306, socket: MYSQLI_CLIENT_SSL)){
+   echo "connection succesfull";
+}
 var_dump($con);
 ?>
