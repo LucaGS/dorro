526322ed26c14f07934c322e86e9b08e8347062d
Luca Stieme <123728529+LucaGS@users.noreply.github.com>
2024-11-28 18:50:09 +0100

    revert changes

--- index.php
+++ index.php
@@ -1,13 +1,14 @@
 <?php
 // Überprüfen, ob der Parameter 'name' in der URL übergeben wurde
-
+if (isset($_GET['name'])) {
+    $name = $_GET['name'];  // Den Wert des Parameters 'name' auslesen
+    echo "Hallo, " . htmlspecialchars($name) . "!";
+} else {
+    echo "Bitte gib deinen Namen in der URL an, z. B. ?name=John";
+}
 
 $con = mysqli_init();
 mysqli_ssl_set(mysql: $con,key: NULL,certificate: NULL, ca_certificate: "C:\dev\dorro\DigiCertGlobalRootCA.crt.pem", ca_path: NULL, cipher_algos: NULL);
-if(mysqli_real_connect(mysql: $conn, hostname: "dorro-server.mysql.database.azure.com", username: "svvaolmvjl", password: "{V6G52dgRBBQd$64$}", database: "{d-database}", port: 3306, socket: MYSQLI_CLIENT_SSL)){
-   echo "connection succesfull";
-}else{
-    echo "Connection not succesfull";
-}
+if(mysqli_real_connect(mysql: $conn, hostname: "dorro-server.mysql.database.azure.com", username: "svvaolmvjl", password: "{V6G52dgRBBQd$64$}", database: "{d-database}", port: 3306, socket: MYSQLI_CLIENT_SSL)){}
 var_dump($con);
 ?>
