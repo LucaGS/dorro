5362ceb5f38963911d71c164a1fbc0b4f0284c29
Luca Stieme <123728529+LucaGS@users.noreply.github.com>
2024-11-30 19:08:09 +0100

    rever

---
+++ config/config.php
@@ -0,0 +1,14 @@
+<?php
+// config/config.php
+
+// Datenbank-Konfiguration
+define('DB_HOST', 'dorro-server.mysql.database.azure.com');  // Hostname, z. B. localhost
+define('DB_NAME', 'dorro');  // Datenbankname
+define('DB_USER', 'svvaolmvjl');  // Datenbankbenutzername
+define('DB_PASS', 'V6G52dgRBBQd$64$');  // Datenbankpasswort
+
+// Setze die Standard-Zeichenkodierung auf UTF-8
+define('DB_CHARSET', 'utf8mb4');
+
+// Weitere Konfigurationswerte können hier hinzugefügt werden, wie API-Keys, Umgebungsvariablen, etc.
+?>
