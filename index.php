0efe85a9adc5d28a85232037998464846c12430d
Luca Stieme <123728529+LucaGS@users.noreply.github.com>
2024-11-30 18:54:12 +0100

    123

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ if (isset($_GET['name'])) {
 }
 
 $con = mysqli_init();
-mysqli_ssl_set(mysql: $con,key: NULL,certificate: NULL, ca_certificate: "C:\dev\dorro\DigiCertGlobalRootCA.crt.pem", ca_path: NULL, cipher_algos: NULL);
+mysqli_ssl_set(mysql: $con,key: NULL,certificate: NULL, ca_certificate: __DIR__+"/DigiCertGlobalRootCA.crt.pem", ca_path: NULL, cipher_algos: NULL);
 if(mysqli_real_connect(mysql: $conn, hostname: "dorro-server.mysql.database.azure.com", username: "svvaolmvjl", password: "V6G52dgRBBQd$64$", database: "d-database", port: 3306, socket: MYSQLI_CLIENT_SSL)){}
 var_dump($con);
 ?>
