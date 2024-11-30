1a33b3c06bbb386db97dfdaaed811303c19bc06d
Luca Stieme <123728529+LucaGS@users.noreply.github.com>
2024-11-30 18:57:42 +0100

    test database connection

--- index.php
+++ index.php
@@ -1,14 +1,56 @@
 <?php
-// Überprüfen, ob der Parameter 'name' in der URL übergeben wurde
-if (isset($_GET['name'])) {
-    $name = $_GET['name'];  // Den Wert des Parameters 'name' auslesen
-    echo "Hallo, " . htmlspecialchars($name) . "!";
+
+
+// Verbindung initialisieren
+$con = mysqli_init();
+mysqli_ssl_set(
+    $con,
+    NULL, // Key
+    NULL, // Zertifikat
+    __DIR__ . "/DigiCertGlobalRootCA.crt.pem", // CA-Zertifikat
+    NULL, // CA-Pfad
+    NULL  // Verschlüsselungsalgorithmus
+);
+
+// Verbindung herstellen
+if (mysqli_real_connect(
+    $con,
+    "dorro-server.mysql.database.azure.com", // Hostname
+    "svvaolmvjl",                           // Benutzername
+    "V6G52dgRBBQd$64$",                     // Passwort
+    "d-database",                           // Datenbankname
+    3306,                                   // Port
+    MYSQLI_CLIENT_SSL                       // SSL-Verbindung
+)) {
+    echo "Erfolgreich mit der Datenbank verbunden!<br>";
+
+    // SQL-Abfrage, um alle Daten aus der User-Tabelle abzurufen
+    $query = "SELECT * FROM User";
+    $result = mysqli_query($con, $query);
+
+    if ($result) {
+        // Ergebnisse ausgeben
+        echo "<table border='1'>";
+        echo "<tr><th>UserId</th><th>Username</th><th>Password</th><th>Email</th><th>CreatedAt</th></tr>";
+
+        while ($row = mysqli_fetch_assoc($result)) {
+            echo "<tr>";
+            echo "<td>" . htmlspecialchars($row['UserId']) . "</td>";
+            echo "<td>" . htmlspecialchars($row['Username']) . "</td>";
+            echo "<td>" . htmlspecialchars($row['Password']) . "</td>";
+            echo "<td>" . htmlspecialchars($row['Email']) . "</td>";
+            echo "<td>" . htmlspecialchars($row['CreatedAt']) . "</td>";
+            echo "</tr>";
+        }
+
+        echo "</table>";
+    } else {
+        echo "Fehler bei der Abfrage: " . mysqli_error($con);
+    }
 } else {
-    echo "Bitte gib deinen Namen in der URL an, z. B. ?name=John";
+    echo "Fehler bei der Verbindung zur Datenbank: " . mysqli_connect_error();
 }
 
-$con = mysqli_init();
-mysqli_ssl_set(mysql: $con,key: NULL,certificate: NULL, ca_certificate: __DIR__+"/DigiCertGlobalRootCA.crt.pem", ca_path: NULL, cipher_algos: NULL);
-if(mysqli_real_connect(mysql: $conn, hostname: "dorro-server.mysql.database.azure.com", username: "svvaolmvjl", password: "V6G52dgRBBQd$64$", database: "d-database", port: 3306, socket: MYSQLI_CLIENT_SSL)){}
-var_dump($con);
+// Verbindung schließen
+mysqli_close($con);
 ?>
