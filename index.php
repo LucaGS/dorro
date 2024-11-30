43a8a33b829b457c28bd5d0cc359aa0a9dbc42e1
Luca Stieme <123728529+LucaGS@users.noreply.github.com>
2024-11-30 19:27:13 +0100

    fsdsdf

--- index.php
+++ index.php
@@ -1,13 +1,4 @@
 <?php
-require_once __DIR__ . '/config/database.php';
-require_once __DIR__ . '/app/controllers/UserController.php';
-
-// Datenbankverbindung herstellen
-$db = getDatabaseConnection();
-
-// Controller initialisieren
-$userController = new UserController($db);
 echo 'test';
-// Benutzerliste anzeigen
-$userController->listUsers();
+
 ?>
