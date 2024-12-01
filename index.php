75c78089a04f2a099ac151adb5a08f6df35a5337
Luca Stieme <123728529+LucaGS@users.noreply.github.com>
2024-12-01 16:44:09 +0100

    small routing fix

--- index.php
+++ index.php
@@ -3,13 +3,7 @@ require_once('/app/views/response.php');
 if ($_SERVER['REQUEST_METHOD'] === 'GET' && isset($_GET['action'])) {
     switch ($_GET['action']) {
         case 'getUser':
-            // Transaktionshistorie abrufen
-            if (isset($_GET['user_id'])) {
-                $user_id = $_GET['user_id'];
                 sendResponse("found", 'user123123', 'userID');
-            } else {
-                sendResponse('error', 'Benutzer-ID fehlt');
-            }
             break;
 
         default:
