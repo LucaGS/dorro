fe5480fa427becb97840626c32bcb1eb750449da
Luca Stieme <123728529+LucaGS@users.noreply.github.com>
2024-12-01 16:54:11 +0100

    fds

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php
-require_once(__DIR__+'/app/views/response.php');
 if ($_SERVER['REQUEST_METHOD'] === 'GET' && isset($_GET['action'])) {
     switch ($_GET['action']) {
         case 'getUser':
