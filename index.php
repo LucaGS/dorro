2106a74da29b3571ba7e13f9dd09b25e10209d70
Luca Stieme <123728529+LucaGS@users.noreply.github.com>
2024-12-01 16:41:25 +0100

    fds

--- index.php
+++ index.php
@@ -1,56 +1,20 @@
 <?php
-// Überprüfen, ob der Parameter 'name' in der URL übergeben wurde
+require_once('/app/views/response.php');
+if ($_SERVER['REQUEST_METHOD'] === 'GET' && isset($_GET['action'])) {
+    switch ($_GET['action']) {
+        case 'getUser':
+            // Transaktionshistorie abrufen
+            if (isset($_GET['user_id'])) {
+                $user_id = $_GET['user_id'];
+                sendResponse("found", 'user123123', 'userID');
+            } else {
+                sendResponse('error', 'Benutzer-ID fehlt');
+            }
+            break;
 
-// Verbindung initialisieren
-$con = mysqli_init();
-mysqli_ssl_set(
-    $con,
-    NULL, // Key
-    NULL, // Zertifikat
-    __DIR__ . "/DigiCertGlobalRootCA.crt.pem", // CA-Zertifikat
-    NULL, // CA-Pfad
-    NULL  // Verschlüsselungsalgorithmus
-);
-
-// Verbindung herstellen
-if (mysqli_real_connect(
-    $con,
-    "dorro-server.mysql.database.azure.com", // Hostname
-    "svvaolmvjl",                           // Benutzername
-    "V6G52dgRBBQd$64$",                     // Passwort
-    "dorro",                           // Datenbankname
-    3306,                                   // Port
-    MYSQLI_CLIENT_SSL                       // SSL-Verbindung
-)) {
-    echo "Erfolgreich mit der Datenbank verbunden!<br>";
-
-    // SQL-Abfrage, um alle Daten aus der User-Tabelle abzurufen
-    $query = "SELECT * FROM User";
-    $result = mysqli_query($con, $query);
-
-    if ($result) {
-        // Ergebnisse ausgeben
-        echo "<table border='1'>";
-        echo "<tr><th>UserId</th><th>Username</th><th>Password</th><th>Email</th><th>CreatedAt</th></tr>";
-
-        while ($row = mysqli_fetch_assoc($result)) {
-            echo "<tr>";
-            echo "<td>" . htmlspecialchars($row['UserId']) . "</td>";
-            echo "<td>" . htmlspecialchars($row['Username']) . "</td>";
-            echo "<td>" . htmlspecialchars($row['Password']) . "</td>";
-            echo "<td>" . htmlspecialchars($row['Email']) . "</td>";
-            echo "<td>" . htmlspecialchars($row['CreatedAt']) . "</td>";
-            echo "</tr>";
-        }
-
-        echo "</table>";
-    } else {
-        echo "Fehler bei der Abfrage: " . mysqli_error($con);
+        default:
+            sendResponse('error', 'Unbekannte Aktion');
+            break;
     }
-} else {
-    echo "Fehler bei der Verbindung zur Datenbank: " . mysqli_connect_error();
 }
-
-// Verbindung schließen
-mysqli_close($con);
 ?>
