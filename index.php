4776eb538698d747e0440992a025504f8ec07200
Luca Stieme <123728529+LucaGS@users.noreply.github.com>
2024-12-01 16:50:34 +0100

    fcdfs

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-require_once('/app/views/response.php');
+require_once(__DIR__+'/app/views/response.php');
 if ($_SERVER['REQUEST_METHOD'] === 'GET' && isset($_GET['action'])) {
     switch ($_GET['action']) {
         case 'getUser':
