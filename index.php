f0eca886bb78edd2f2785f817f12ff3f0fd14020
Luca Stieme <luca.stieme@outlook.de>
2024-12-02 08:44:41 +0100

    fdfd

--- index.php
+++ index.php
@@ -1,9 +1,11 @@
 <?php
 require_once __DIR__ . '/config/database.php';
 require_once __DIR__ . '/app/controller/userController.php';
+require_once __DIR__ . '/app/Models/userModel.php';
 
 $conn = getDatabaseConnection();
-$userController = new UserController($conn);
+$userModel = new UserModel($conn);
+$userController = new UserController($userModel);
 if ($conn) {
     echo "Datenbankverbindung erfolgreich!";
     $userController->listUsers();
