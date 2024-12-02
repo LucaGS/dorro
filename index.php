0381e95c4757db3c426c2219d6f9e8f57719e40b
Luca Stieme <luca.stieme@outlook.de>
2024-12-02 08:30:56 +0100

    fads

--- index.php
+++ index.php
@@ -4,10 +4,8 @@ require_once __DIR__ . '/app/controller/userController.php';
 require_once __DIR__ . '/app/Models/userModel.php';
 
 $conn = getDatabaseConnection();
-#$userController = new UserController($con);
 if ($conn) {
     echo "Datenbankverbindung erfolgreich!";
-    #$userController->listUsers();
 } else {
     echo "Verbindung fehlgeschlagen.";
 }
