26c098b6dfdb806fbe73c000d700895262bfc80f
Luca Stieme <luca.stieme@outlook.de>
2024-12-02 08:38:23 +0100

    userController

--- index.php
+++ index.php
@@ -3,6 +3,7 @@ require_once __DIR__ . '/config/database.php';
 require_once __DIR__ . '/app/controller/userController.php';
 
 $conn = getDatabaseConnection();
+$userController = new UserController($conn);
 if ($conn) {
     echo "Datenbankverbindung erfolgreich!";
 } else {
