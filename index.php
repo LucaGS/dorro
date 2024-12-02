c9254b2920677e3d901a73659480e092dde2d9e1
Luca Stieme <123728529+LucaGS@users.noreply.github.com>
2024-12-02 15:40:27 +0100

    post update
    afsdf

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <?php
 require_once __DIR__ . '/config/database.php';
 require_once __DIR__ . '/app/controller/userController.php';
-
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 $conn = getDatabaseConnection();
 $userController = new UserController($conn);
 
