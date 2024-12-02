adbda30399e747daa0bd1bbd7a21353220cfb4f9
Luca Stieme <luca.stieme@outlook.de>
2024-12-02 08:33:23 +0100

    fadf

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <?php
 require_once __DIR__ . '/config/database.php';
-require_once __DIR__ . '/app/controller/userController.php';
-require_once __DIR__ . '/app/Models/userModel.php';
+
 
 $conn = getDatabaseConnection();
 if ($conn) {
