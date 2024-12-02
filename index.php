2c0995f16cca7259eb86050f97dfb313cf8b989a
Stieme <StiemLuc@edu-it.local>
2024-12-02 09:08:00 +0100

    adsfasdf

--- index.php
+++ index.php
@@ -4,12 +4,7 @@ require_once __DIR__ . '/app/controller/userController.php';
 
 $conn = getDatabaseConnection();
 $userController = new UserController($conn);
-if ($conn) {
-    echo "Datenbankverbindung erfolgreich!";
-    $userController->listUsers();
-} else {
-    echo "Verbindung fehlgeschlagen.";
-}
+
 if ($_SERVER['REQUEST_METHOD'] == 'GET' && isset($_GET['action'])) {
     $action = $_GET['action'];
     switch ($action) {
