f7b92cca50c24094027be6b851773f12acf086a0
Luca Stieme <123728529+LucaGS@users.noreply.github.com>
2024-12-14 14:46:48 +0100

    fix to routine adder

--- app/Models/routineModel.php
+++ app/Models/routineModel.php
@@ -9,7 +9,7 @@ class RoutineModel{
     {
         $this->db = $db;
     }
-    public function createRoutine($routine_name,$routine_type, $user_id, $description = "description"){
+    public function createRoutine($routine_name,$routine_type ="default", $user_id, $description = "description"){
        $query = "INSERT INTO routines(routine_name,type,user_id,description) values(?,?,?,?)";  
        $stmt = mysqli_prepare($this->db, $query);
        mysqli_stmt_bind_param($stmt, 'ssis', $routine_name, $routine_type, $user_id, $description);
